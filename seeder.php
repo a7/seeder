d6b9e83956fb9e556a85afdb660116c5cff5aa4f
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2017-11-22 06:56:00 -0600

    Update seeder.php

--- seeder.php
+++ seeder.php
@@ -4,18 +4,4 @@ namespace A7\Seeder;
 
 use function A7\autoload;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die( 'Cannot access pages directly.' );
-}
-
-// If used in multiple places, only load once
-if ( function_exists( '\A7\Seeder\do_seeding' ) ) {
-	return;
-}
-
-/**
- * Load all the composer packages
- */
-require_once __DIR__ . '/vendor/autoload.php';
-
 autoload( __DIR__ . '/src' );
